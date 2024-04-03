c54eb6c19c3e84a06d8815eece8c65f41ee84136
Deepesh Alwani <42600450+DeepeshAlwani@users.noreply.github.com>
2024-04-03 13:24:53 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,8 +4,6 @@ sudo yum install -y amazon-linux-extras
 sudo amazon-linux-extras install epel -y
 sudo yum-config-manager --enable epel
 sudo yum install -y git-lfs
-y
-y
 git lfs install
 git lfs version
 
