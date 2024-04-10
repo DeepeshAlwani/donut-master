23e72707e6df00ad6d65b52393b5926160d1a06b
Deepesh Alwani <42600450+DeepeshAlwani@users.noreply.github.com>
2024-04-10 12:15:43 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -15,7 +15,7 @@ pip install -r donut-master/requirment.txt
 
 DOWNLOAD_FILE="donut-master/download_training_data.py"
 
-python "$DOWNLOAD_FILE" $1 $2 $4
+python "$DOWNLOAD_FILE" $1 $2 $3
 
 DATASET_FILE="donut-master/run_donut.py"
 
