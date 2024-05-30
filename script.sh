475417f198a75b54fb6807bd083b562556cde606
Deepesh Alwani <42600450+DeepeshAlwani@users.noreply.github.com>
2024-05-30 16:15:53 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -22,8 +22,8 @@ if [ $4 == "donut_invoice" ]; then
 
     # Run specific scripts for 'donut-dwg'
 elif [ $4 == "donut_dwg" ]; then
-    GENERATE_DATA = "donut-master/generate_data_for_dwg.py"
-    python "$GENERATE_DATA" $1 $2
+    GENERATE_FILE_DWG = "donut-master/generate_data_for_dwg.py"
+    python "$GENERATE_FILE_DWG" $1 $2
     DOWNLOAD_FILE_DWG="donut-master/download_data_for_training_dwg.py"
     python "$DOWNLOAD_FILE_DWG" $1 $2 $3
 else
