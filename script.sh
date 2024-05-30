99be4cab7f0926bfcc2f2a047b371ac5e7a7b667
Deepesh Alwani <42600450+DeepeshAlwani@users.noreply.github.com>
2024-05-30 11:43:26 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # Path to the requirements.txt file
 REQUIREMENTS_FILE="donut-master/requirment.txt"
 
@@ -11,30 +10,28 @@ if [ ! -f "$REQUIREMENTS_FILE" ]; then
 fi
 
 # Install libraries listed in requirements.txt
-pip install -r donut-master/requirment.txt
+pip install -r "$REQUIREMENTS_FILE"
 
 arg4=$4
 
 # Conditional statements to check the value of the 4th argument
 if [ "$arg4" == "donut_invoice" ]; then
-  # Run specific scripts for 'donut-invoice'
+    # Run specific scripts for 'donut-invoice'
     DOWNLOAD_FILE_INVOICE="donut-master/download_training_data.py"
-    python "$DOWNLOAD_FILE_INVOICE" $1 $2 $3
+    python "$DOWNLOAD_FILE_INVOICE" "$1" "$2" "$3"
 elif [ "$arg4" == "donut_dwg" ]; then
-  # Run specific scripts for 'donut-dwg'
-      GENERATE_FILE_DWG = "donut-master/generate_data_for_dwg.py"
-      python "$GENERATE_FILE_DWG" $1 $2
-      DOWNLOAD_FILE_DWG = "donut-master/donwload_data_for_training_dwg.py"
-      pythonn "$DOWNLOAD_FILE_DWG" $1 $2 $3
-      
+    # Run specific scripts for 'donut-dwg'
+    GENERATE_FILE_DWG="donut-master/generate_data_for_dwg.py"
+    python "$GENERATE_FILE_DWG" "$1" "$2"
+    DOWNLOAD_FILE_DWG="donut-master/donwload_data_for_training_dwg.py"
+    python "$DOWNLOAD_FILE_DWG" "$1" "$2" "$3"
 else
-  echo "Invalid argument provided. Please use 'donut_invoice' or 'donut_dwg' as the 4th argument."
+    echo "Invalid argument provided. Please use 'donut_invoice' or 'donut_dwg' as the 4th argument."
+    exit 1
 fi
 
 DATASET_FILE="donut-master/run_donut.py"
-
 python "$DATASET_FILE"
 
 TRAINER_FILE="donut-master/donut_training.py"
-
-python "$TRAINER_FILE" $4
+python "$TRAINER_FILE" "$4"
