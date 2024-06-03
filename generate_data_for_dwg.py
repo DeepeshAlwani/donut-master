572fb65ae80ff4fe2af48ee7283759ac39ace7f7
Deepesh Alwani <42600450+DeepeshAlwani@users.noreply.github.com>
2024-06-03 17:31:34 +0530

    Update generate_data_for_dwg.py

--- generate_data_for_dwg.py
+++ generate_data_for_dwg.py
@@ -4,25 +4,21 @@ import json
 import requests
 import urllib.parse
 import io
-import requests
 from PIL import Image
-from pdf2image import convert_from_bytes # PyMuPDF
+from pdf2image import convert_from_bytes
 from boto3.dynamodb.conditions import Attr
 
 DYNAMODB_TABLE_NAME = 'DwgHdrInfo'
-column_name = 'vendorName'
 AWS_REGION = 'us-east-1'
-download_folder = 'downloadfiles'
 S3_BUCKET_NAME = 'cverse-dev'
 S3_BUCKET_NAME1 = 'cf-ai-test2'
+
 if len(sys.argv) != 3:
-    print("Usage: python script.py <json_path>")
+    print("Usage: python script.py <AWS_ACCESS_KEY_ID> <AWS_SECRET_ACCESS_KEY>")
     sys.exit(1)
+
 AWS_ACCESS_KEY_ID = sys.argv[1]
 AWS_SECRET_ACCESS_KEY = sys.argv[2]
-AWS_REGION = 'us-east-1'
-
-
 
 def get_pdf_s3_url():
     dynamodb = boto3.resource('dynamodb', region_name=AWS_REGION, 
@@ -44,31 +40,29 @@ def get_pdf_s3_url():
             'title_val': item.get('titleVal'),
             'page_num': item.get('pageNum'),
             'doc_id': item.get('docId')
-            #'documentType': item.get('documentType')
         })
     return s3url_org_doc_ids
 
 def generate_signed_url(url, expiration=3600):
-        #parsed_url = urlparse(url)
-        position = url.find('//', 8) + 2  # Adding 2 to include the double slashes
-        object_key = url[position:]
-        print(object_key)
-        object_key = urllib.parse.unquote(object_key)
-        print(object_key)
-        object_key = "/" + object_key
-        filename = object_key.split("/")[-1]
-        s3_client = boto3.client('s3', region_name=AWS_REGION, aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+    position = url.find('//', 8) + 2  # Adding 2 to include the double slashes
+    object_key = url[position:]
+    print(object_key)
+    object_key = urllib.parse.unquote(object_key)
+    print(object_key)
+    object_key = "/" + object_key
+    filename = object_key.split("/")[-1]
+    s3_client = boto3.client('s3', region_name=AWS_REGION, aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 
-        # Generate a pre-signed URL for the S3 object
-        presigned_url = s3_client.generate_presigned_url(
-            'get_object',
-            Params={
-                'Bucket': S3_BUCKET_NAME,
-                'Key': object_key,
-            },
-            ExpiresIn= expiration
-        )
-        return presigned_url, filename
+    # Generate a pre-signed URL for the S3 object
+    presigned_url = s3_client.generate_presigned_url(
+        'get_object',
+        Params={
+            'Bucket': S3_BUCKET_NAME,
+            'Key': object_key,
+        },
+        ExpiresIn=expiration
+    )
+    return presigned_url, filename
 
 def upload_file_to_s3(filename, data, pageNum, docId):
     s3 = boto3.client('s3', region_name=AWS_REGION, aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
@@ -149,9 +143,8 @@ def create_jpeg_and_upload_to_s3(pdf_bytes, pageNum, docId, filename):
     print(jpeg_url_presigned)
 
     return jpeg_url
-    
 
-def update_data_in_dynamodb(pageNum,docId, updated_s3_url, jpeg_url,json_url):
+def update_data_in_dynamodb(pageNum, docId, updated_s3_url, jpeg_url, json_url):
     dynamodb = boto3.resource('dynamodb', region_name=AWS_REGION, aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
     table = dynamodb.Table(DYNAMODB_TABLE_NAME)
 
@@ -175,11 +168,11 @@ def update_data_in_dynamodb(pageNum,docId, updated_s3_url, jpeg_url,json_url):
 
     return response
 
-
 s3url_org_doc_ids = get_pdf_s3_url()
-if len(s3url_org_doc_ids) <1:
-    print("No records to fetch exiting the script")
+if len(s3url_org_doc_ids) < 1:
+    print("No records to fetch. Exiting the script.")
     exit()
+
 for dict in s3url_org_doc_ids:
     pageNum = dict["page_num"]
     docId = dict["doc_id"]
@@ -188,17 +181,21 @@ for dict in s3url_org_doc_ids:
     signed_url, filename = generate_signed_url(dict["s3Url"])
     #print(signed_url)
     try:
-        response = requests.get(signed_url)
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        }
+        response = requests.get(signed_url, headers=headers, stream=True, verify=False)
         response.raise_for_status()
-        updated_s3_url =  upload_file_to_s3(filename, response.content, pageNum, docId)
+        updated_s3_url = upload_file_to_s3(filename, response.content, pageNum, docId)
         pdf_bytes_io = io.BytesIO(response.content)
         pdf_bytes = pdf_bytes_io.read()
-        jpeg_url = create_jpeg_and_upload_to_s3(pdf_bytes, pageNum, docId,filename)
+        jpeg_url = create_jpeg_and_upload_to_s3(pdf_bytes, pageNum, docId, filename)
         json_url = create_json_data_and_upload_to_s3(titleVal, numVal, docId, pageNum, filename)
-        request = update_data_in_dynamodb(pageNum,docId, updated_s3_url, jpeg_url,json_url)
+        request = update_data_in_dynamodb(pageNum, docId, updated_s3_url, jpeg_url, json_url)
         print(request)
         
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+        print(f"Response content: {response.content}")
     except Exception as e:
-        pass
-        print(f"Failed to complete the request: {e}")
-        
+        print(f"An error occurred: {e}")
