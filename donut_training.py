5200f226f6e2362a3222e52ffa22e172497bee31
Deepesh Alwani <42600450+DeepeshAlwani@users.noreply.github.com>
2024-07-01 11:40:45 +0530

    Update donut_training.py

--- donut_training.py
+++ donut_training.py
@@ -1,53 +1,50 @@
 import sys
-import os
-import json
-import random
-import re
-import numpy as np
-from typing import Any, List, Tuple
-from ast import literal_eval
-from pathlib import Path
+
+pretrained_model_path = sys.argv[1]
+print(pretrained_model_path)
+
 from datasets import load_dataset
-from nltk import edit_distance
 
-import torch
-from torch.utils.data import Dataset, DataLoader
-from transformers import VisionEncoderDecoderConfig, DonutProcessor, VisionEncoderDecoderModel, AutoTokenizer
-import pytorch_lightning as pl
-from pytorch_lightning.utilities import rank_zero_only
-from pytorch_lightning.callbacks import Callback, EarlyStopping, ModelCheckpoint
-from pytorch_lightning.loggers import TensorBoardLogger
-from datetime import datetime
-
-# Verify model name argument
-if len(sys.argv) < 2:
-    print("Please provide the model name you want to train: donut-invoice or donut-dwg")
-    sys.exit(1)
-model_name = sys.argv[1]
-pretrained_model_path = model_name
-
-# Load dataset
-if not os.path.isdir("dataset"):
-    print("Directory 'dataset' does not exist.")
-    sys.exit(1)
 dataset = load_dataset("dataset")
+dataset
 example = dataset['train'][0]
 image = example['image']
+# let's make the image a bit smaller when visualizing
 width, height = image.size
+#display(image.resize((int(width*0.3), int(height*0.3))))
+# let's load the corresponding JSON dictionary (as string representation)
 ground_truth = example['ground_truth']
 print(ground_truth)
-literal_eval(ground_truth)['gt_parse']
 
-# Configure model
+from ast import literal_eval
+
+literal_eval(ground_truth)['gt_parse']
+from transformers import VisionEncoderDecoderConfig
+
 image_size = [1275, 1650]
 max_length = 768
+
+# update image_size of the encoder
+# during pre-training, a larger image size was used
 config = VisionEncoderDecoderConfig.from_pretrained(pretrained_model_path)
-config.encoder.image_size = image_size
+config.encoder.image_size = image_size # (height, width)
+# update max_length of the decoder (for generation)
 config.decoder.max_length = max_length
+
+# TODO we should actually update max_position_embeddings and interpolate the pre-trained ones:
+# https://github.com/clovaai/donut/blob/0acc65a85d140852b8d9928565f0f6b2d98dc088/donut/model.py#L602
+from transformers import DonutProcessor, VisionEncoderDecoderModel, AutoTokenizer
 
 processor = DonutProcessor.from_pretrained(pretrained_model_path)
 model = VisionEncoderDecoderModel.from_pretrained(pretrained_model_path, config=config)
 tokenizer = AutoTokenizer.from_pretrained(pretrained_model_path)
+import json
+import random
+from typing import Any, List, Tuple
+
+import torch
+from torch.utils.data import Dataset
+
 added_tokens = []
 
 class DonutDataset(Dataset):
@@ -189,32 +186,46 @@ class DonutDataset(Dataset):
         # labels[: torch.nonzero(labels == self.prompt_end_token_id).sum() + 1] = self.ignore_id  # model doesn't need to predict prompt (for VQA)
         return pixel_values, labels, target_sequence
 
-processor.image_processor.size = image_size[::-1]
+# we update some settings which differ from pretraining; namely the size of the images + no rotation required
+# source: https://github.com/clovaai/donut/blob/master/config/train_cord.yaml
+processor.image_processor.size = image_size[::-1] # should be (width, height)
 processor.image_processor.do_align_long_axis = False
 
-train_dataset = DonutDataset("dataset", max_length=max_length, split="train",
-                             task_start_token="<s_cord-v2>", prompt_end_token="<s_cord-v2>", sort_json_key=False)
-val_dataset = DonutDataset("dataset", max_length=max_length, split="validation",
-                           task_start_token="<s_cord-v2>", prompt_end_token="<s_cord-v2>", sort_json_key=False)
+train_dataset = DonutDataset("dataset", max_length=max_length,
+                             split="train", task_start_token="<s_cord-v2>", prompt_end_token="<s_cord-v2>",
+                             sort_json_key=False, # cord dataset is preprocessed, so no need for this
+                             )
 
+val_dataset = DonutDataset("dataset", max_length=max_length,
+                             split="validation", task_start_token="<s_cord-v2>", prompt_end_token="<s_cord-v2>",
+                             sort_json_key=False, # cord dataset is preprocessed, so no need for this
+                             )
+len(added_tokens)
 print(added_tokens)
+# the vocab size attribute stays constants (might be a bit unintuitive - but doesn't include special tokens)
 print("Original number of tokens:", processor.tokenizer.vocab_size)
 print("Number of tokens after adding special tokens:", len(processor.tokenizer))
 processor.decode([57521])
-
-# Print sample data
 pixel_values, labels, target_sequence = train_dataset[0]
 print(pixel_values.shape)
+# let's print the labels (the first 30 token ID's)
 for id in labels.tolist()[:30]:
-    print(processor.decode([id]) if id != -100 else id)
+  if id != -100:
+    print(processor.decode([id]))
+  else:
+    print(id)
+# let's check the corresponding target sequence, as a string
 print(target_sequence)
-
 model.config.pad_token_id = processor.tokenizer.pad_token_id
 model.config.decoder_start_token_id = processor.tokenizer.convert_tokens_to_ids(['<s_cord-v2>'])[0]
+# sanity check
 print("Pad token ID:", processor.decode([model.config.pad_token_id]))
 print("Decoder start token ID:", processor.decode([model.config.decoder_start_token_id]))
 
-# Dataloaders
+from torch.utils.data import DataLoader
+
+# feel free to increase the batch size if you have a lot of memory
+# I'm fine-tuning on Colab and given the large image size, batch size > 1 is not feasible
 train_dataloader = DataLoader(train_dataset, batch_size=1, shuffle=True, num_workers=0)
 val_dataloader = DataLoader(val_dataset, batch_size=1, shuffle=False, num_workers=0)
 print(train_dataloader)
@@ -222,13 +233,29 @@ batch = next(iter(train_dataloader))
 pixel_values, labels, target_sequences = batch
 print(pixel_values.shape)
 for id in labels.squeeze().tolist()[:30]:
-    print(processor.decode([id]) if id != -100 else id)
+  if id != -100:
+    print(processor.decode([id]))
+  else:
+    print(id)
 print(len(train_dataset))
 print(len(val_dataset))
+# let's check the first validation batch
 batch = next(iter(val_dataloader))
 pixel_values, labels, target_sequences = batch
 print(pixel_values.shape)
 print(target_sequences[0])
+from pathlib import Path
+import re
+from nltk import edit_distance
+import numpy as np
+import math
+
+from torch.nn.utils.rnn import pad_sequence
+from torch.optim.lr_scheduler import LambdaLR
+
+import pytorch_lightning as pl
+from pytorch_lightning.utilities import rank_zero_only
+
 
 class DonutModelPLModule(pl.LightningModule):
     def __init__(self, config, processor, model):
@@ -239,6 +266,7 @@ class DonutModelPLModule(pl.LightningModule):
 
     def training_step(self, batch, batch_idx):
         pixel_values, labels, _ = batch
+        
         outputs = self.model(pixel_values, labels=labels)
         loss = outputs.loss
         self.log("train_loss", loss)
@@ -247,81 +275,110 @@ class DonutModelPLModule(pl.LightningModule):
     def validation_step(self, batch, batch_idx, dataset_idx=0):
         pixel_values, labels, answers = batch
         batch_size = pixel_values.shape[0]
+        # we feed the prompt to the model
         decoder_input_ids = torch.full((batch_size, 1), self.model.config.decoder_start_token_id, device=self.device)
-        outputs = self.model.generate(pixel_values, decoder_input_ids=decoder_input_ids, max_length=max_length,
-                                      early_stopping=True, pad_token_id=self.processor.tokenizer.pad_token_id,
-                                      eos_token_id=self.processor.tokenizer.eos_token_id, use_cache=True, num_beams=1,
-                                      bad_words_ids=[[self.processor.tokenizer.unk_token_id]], return_dict_in_generate=True)
-        predictions = [
-            re.sub(r"<.*?>", "", seq.replace(self.processor.tokenizer.eos_token, "").replace(self.processor.tokenizer.pad_token, ""), count=1).strip()
-            for seq in self.processor.tokenizer.batch_decode(outputs.sequences)
-        ]
-        scores = [
-            edit_distance(pred, answer) / max(len(pred), len(answer))
-            for pred, answer in zip(predictions, answers)
-        ]
-        if self.config.get("verbose", False) and scores:
-            print(f"Prediction: {predictions[0]}")
-            print(f"    Answer: {answers[0]}")
-            print(f" Normed ED: {scores[0]}")
+        
+        outputs = self.model.generate(pixel_values,
+                                   decoder_input_ids=decoder_input_ids,
+                                   max_length=max_length,
+                                   early_stopping=True,
+                                   pad_token_id=self.processor.tokenizer.pad_token_id,
+                                   eos_token_id=self.processor.tokenizer.eos_token_id,
+                                   use_cache=True,
+                                   num_beams=1,
+                                   bad_words_ids=[[self.processor.tokenizer.unk_token_id]],
+                                   return_dict_in_generate=True,)
+    
+        predictions = []
+        for seq in self.processor.tokenizer.batch_decode(outputs.sequences):
+            seq = seq.replace(self.processor.tokenizer.eos_token, "").replace(self.processor.tokenizer.pad_token, "")
+            seq = re.sub(r"<.*?>", "", seq, count=1).strip()  # remove first task start token
+            predictions.append(seq)
+
+        scores = []
+        for pred, answer in zip(predictions, answers):
+            pred = re.sub(r"(?:(?<=>) | (?=</s_))", "", pred)
+            # NOT NEEDED ANYMORE
+            # answer = re.sub(r"<.*?>", "", answer, count=1)
+            answer = answer.replace(self.processor.tokenizer.eos_token, "")
+            scores.append(edit_distance(pred, answer) / max(len(pred), len(answer)))
+
+            if self.config.get("verbose", False) and len(scores) == 1:
+                print(f"Prediction: {pred}")
+                print(f"    Answer: {answer}")
+                print(f" Normed ED: {scores[0]}")
+
         self.log("val_edit_distance", np.mean(scores))
+        
         return scores
 
     def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(), lr=self.config.get("lr"))
+        # you could also add a learning rate scheduler if you want
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.config.get("lr"))
+    
+        return optimizer
 
     def train_dataloader(self):
         return train_dataloader
 
     def val_dataloader(self):
         return val_dataloader
+config = {"max_epochs":4,
+          "val_check_interval":0.5, # how many times we want to validate during an epoch
+          "check_val_every_n_epoch":1,
+          "gradient_clip_val":1.0,
+          "num_training_samples_per_epoch": 40,
+          "lr":3e-5,
+          "train_batch_sizes": [1],
+          "val_batch_sizes": [1],
+          # "seed":2022,
+          "num_nodes": 1,
+          "warmup_steps": 12, # 800/8*30/10, 10%
+          "result_path": "home/results",
+          "verbose": True,
+          }
 
-config = {
-    "max_epochs": 12,
-    "val_check_interval": 0.5,
-    "check_val_every_n_epoch": 1,
-    "gradient_clip_val": 1.0,
-    "num_training_samples_per_epoch": 40,
-    "lr": 5e-5,
-    "train_batch_sizes": [1],
-    "val_batch_sizes": [1],
-    "num_nodes": 1,
-    "warmup_steps": 12,
-    "result_path": "home/results",
-    "verbose": True,
-}
 
 model_module = DonutModelPLModule(config, processor, model)
-tensorboard_logger = TensorBoardLogger("finetune_logs", name="donut-cord-v2")
-early_stop_callback = EarlyStopping(monitor="val_edit_distance", patience=12, verbose=True, mode="min")
+import os
+import torch
+torch.cuda.empty_cache()
+from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.callbacks import Callback, EarlyStopping
+import pytorch_lightning as pl
 
-current_date = datetime.now()
-formatted_date = current_date.strftime("%d-%b-%Y")
+# Replace WandbLogger with TensorBoardLogger
+tensorboard_logger = TensorBoardLogger("logs", name="Donut-demo-run-cord")
 
-checkpoint_callback = ModelCheckpoint(
-    monitor="val_edit_distance",
-    dirpath="saved_model",
-    filename=f"{model_name}-{formatted_date}",
-    save_top_k=3,
-    mode="min",
-)
+class PushToHubCallback(Callback):
+	def on_train_epoch_end(self, trainer, pl_module):
+		print(f"Pushing model to the hub, epoch {trainer.current_epoch}")
+		pl_module.model.push_to_hub("DeepeshAlwani/donut_invoice",commit_message=f"Training in progress, epoch {trainer.current_epoch}")
 
+	def on_train_end(self, trainer, pl_module):
+		print(f"Pushing model to the hub after training")
+		pl_module.processor.push_to_hub("DeepeshAlwani/donut_invoice",commit_message=f"Training done")
+		pl_module.model.push_to_hub("DeepeshAlwani/donut_invoice",commit_message=f"Training done")
+
+     
+
+early_stop_callback = EarlyStopping(monitor="val_edit_distance", patience=3, verbose=False, mode="min")
 
 trainer = pl.Trainer(
-    accelerator="gpu", devices=1,
-    precision=16,
-    max_epochs=config["max_epochs"],
-    val_check_interval=config["val_check_interval"],
-    check_val_every_n_epoch=config["check_val_every_n_epoch"],
-    gradient_clip_val=config["gradient_clip_val"],
-    limit_train_batches=config["num_training_samples_per_epoch"],
-    num_nodes=config["num_nodes"],
-    logger=tensorboard_logger,
-    callbacks=[early_stop_callback, checkpoint_callback]
+    accelerator="gpu",
+    devices=1,
+    max_epochs=config.get("max_epochs"),
+    val_check_interval=config.get("val_check_interval"),
+    check_val_every_n_epoch=config.get("check_val_every_n_epoch"),
+    gradient_clip_val=config.get("gradient_clip_val"),
+    precision=16,  # we'll use mixed precision
+    num_sanity_val_steps=0,
+    logger=tensorboard_logger,  # Use TensorBoardLogger instead of WandbLogger
+    callbacks=[PushToHubCallback()],
 )
-print(added_tokens)
+
 trainer.fit(model_module)
 model.save_pretrained("fine_tuned_model")
 tokenizer.add_tokens(added_tokens)
 tokenizer.save_pretrained("fine_tuned_model")
-
+processor.save_pretrained("fine_tuned_model")
